5525e128337883f01868b2a79a9d5571151f1337
reova <github.boykovolodymyr@gmail.com>
2024-03-17 15:27:37 +0200

    Add db tables for goods app and edit goods html

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Category')
+
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Image')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Price')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Discount in %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Quantity')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from goods.models import Categories
+
+
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Home ',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories,
     }
     
     return render(request, 'main/index.html', context)
