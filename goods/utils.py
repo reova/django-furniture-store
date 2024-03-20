3c1089d6ad292fd55ec5fc1799c941cf8cc6cdbf
reova <github.boykovolodymyr@gmail.com>
2024-03-20 16:43:56 +0200

    connect PostgreSQL db & add full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from goods.models import Products
 
@@ -6,14 +7,19 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+
+    vector = SearchVector('name', 'description')
+    query = SearchQuery('')
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    # keywords = [word for word in query.split() if len(word) > 2]
     
-    q_objects = Q()
+    # q_objects = Q()
     
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
     
-    print(q_objects)
-    return Products.objects.filter(q_objects)
+    # print(q_objects)
+    # return Products.objects.filter(q_objects)
--- goods/views.py
+++ goods/views.py
@@ -14,7 +14,7 @@ def catalog(request, category_slug=None):
     
     if category_slug == 'all':
         goods = Products.objects.all()
-    elif query:
+    elif query: 
         goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
