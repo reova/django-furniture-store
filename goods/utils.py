1de0d07d2c52f3e75d2c6c751c68a955c48d9690
reova <github.boykovolodymyr@gmail.com>
2024-03-20 18:14:29 +0200

    Enhance website search functionality

--- goods/utils.py
+++ goods/utils.py
@@ -1,5 +1,4 @@
-from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 
 from goods.models import Products
 
@@ -9,17 +8,28 @@ def q_search(query):
         return Products.objects.filter(id=int(query))
 
     vector = SearchVector('name', 'description')
-    query = SearchQuery('')
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    query = SearchQuery(query)
 
-    
-    # keywords = [word for word in query.split() if len(word) > 2]
-    
-    # q_objects = Q()
-    
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains=token)
-    #     q_objects |= Q(name__icontains=token)
-    
-    # print(q_objects)
-    # return Products.objects.filter(q_objects)
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
