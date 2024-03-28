fc2243e0fcd537eae895a91444ba70719ebfe3e3
reova <github.boykovolodymyr@gmail.com>
2024-03-28 10:18:27 +0200

    add carts sessions for anonymous user

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
+from carts.models import Cart
 
 
 def login(request):
@@ -18,9 +19,14 @@ def login(request):
                 username=username, password=password
             )  # check if the user is in the db
 
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)  # if the user is in the db - authorize
                 messages.success(request, f"{username}, вошли в аккаунт")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -44,8 +50,15 @@ def registration(request):
 
         if form.is_valid():
             form.save()
+            
+            session_key = request.session.session_key
+            
             user = form.instance  # get data from form
             auth.login(request, user)  # authorize user
+            
+            if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+            
             messages.success(request, f"{user.username}, успешно зарегистрировались и вошли в аккаунт")
 
             return HttpResponseRedirect(reverse("main:index"))
